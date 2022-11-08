9c0ad1d6e42cee8d221ae526b4ab84adb38e53a2
Cole Stephenson <colestephenson94@gmail.com>
2022-11-08 14:48:05 -0700

    Write greeting component, and begin CSS

---
+++ src/components/Greeting/Greeting.js
@@ -0,0 +1,11 @@
+import './Greeting.css'
+
+const Greeting = () => {
+    return (
+        <div className='greeting'>
+            <p className='greeting-message'>Welcome! Choose a Topic to Begin.</p>
+        </div>
+    )
+}
+
+export default Greeting;
